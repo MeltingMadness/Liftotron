89ae3aa2d88dfe96fd33e6d9d06647d19c7ec87b
MeltingMadness <124767424+MeltingMadness@users.noreply.github.com>
2023-04-13 10:15:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -162,8 +162,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, check_gm))
     dp.add_error_handler(error_handler)
     
-    jq = JobQueue()
-    jq.set_dispatcher(dp)
+    jq = updater.job_queue
 
     # Zeitzone festlegen
     timezone = pytz.timezone('Europe/Berlin')
